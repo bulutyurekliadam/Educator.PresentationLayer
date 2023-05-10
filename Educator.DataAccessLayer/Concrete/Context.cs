81e19630dcfbdf6dea89f40f109f67d107e8b3b4
bulutyurekliadam <ayvaz.hakan@outlook.com>
2023-05-10 20:06:56 +0300

    Paketler eklendi.

---
+++ Educator.DataAccessLayer/Concrete/Context.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Educator.DataAccessLayer.Concrete
+{
+	public class Context:DbContext
+	{ 
+		protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+		{
+			optionsBuilder.UseSqlServer("Server=DESKTOP-H67U406; initial catalog =DbEdukator; integrated security = true; ");
+
+		}
+	}
+}
