f547528639c39467e43f9ff934aa8309b785c89f
bulutyurekliadam <ayvaz.hakan@outlook.com>
2023-06-07 23:12:26 +0300

    14. ders sonu

--- Educator.DataAccessLayer/Migrations/20230607200201_mig_addidentity.Designer.cs
+++ Educator.DataAccessLayer/Migrations/20230607200201_mig_addidentity.Designer.cs
@@ -4,14 +4,16 @@ using Educator.DataAccessLayer.Concrete;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace Educator.DataAccessLayer.Migrations
 {
     [DbContext(typeof(Context))]
-    partial class ContextModelSnapshot : ModelSnapshot
+    [Migration("20230607200201_mig_addidentity")]
+    partial class mig_addidentity
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
