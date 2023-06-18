777311768cd77561a155f5122e02914886716b6f
bulutyurekliadam <ayvaz.hakan@outlook.com>
2023-06-19 00:27:42 +0300

    Ders 20. Sonu

---
+++ Educator.PresentationLayer/Controllers/UsersController.cs
@@ -0,0 +1,24 @@
+﻿using Educator.EntityLayer.Concrete;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+
+namespace Educator.PresentationLayer.Controllers
+{
+    public class UsersController : Controller
+    {
+        private readonly UserManager<AppUser> _userManager;
+
+        public UsersController(UserManager<AppUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        public IActionResult Index()
+        {
+            var values = _userManager.Users.ToList();
+
+            return View(values);
+        }
+    }
+}
