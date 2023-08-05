81174993e1d8531c23fbc8c89d125e35f7747461
bulutyurekliadam <ayvaz.hakan@outlook.com>
2023-08-05 20:32:06 +0300

    settings

---
+++ Educator.PresentationLayer/Controllers/SettingsController.cs
@@ -0,0 +1,32 @@
+﻿using Educator.EntityLayer.Concrete;
+using Educator.PresentationLayer.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace Educator.PresentationLayer.Controllers
+{
+    public class SettingsController : Controller
+    {
+        private readonly UserManager<AppUser> _userManager;
+
+        public SettingsController(UserManager<AppUser> userManager)
+        {
+            _userManager = userManager;
+        }
+        [HttpGet]
+        public async Task<IActionResult> Index()
+        {
+            var values = await _userManager.FindByNameAsync(User.Identity.Name);
+            UserEditViewModel userEditViewModel = new UserEditViewModel();
+            userEditViewModel.Name = values.Name;
+            userEditViewModel.Surname = values.Surname;
+            userEditViewModel.UserName= values.UserName;
+            userEditViewModel.ImageUrl= values.imageUrl;
+            userEditViewModel.Email= values.Email;
+            return View(userEditViewModel);  
+        }
+
+       
+    }
+}
---
+++ Educator.PresentationLayer/Models/UserEditViewModel.cs
@@ -0,0 +1,14 @@
+﻿namespace Educator.PresentationLayer.Models
+{
+    public class UserEditViewModel
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string UserName { get; set; }
+        public string Email { get; set; }   
+        public string Password { get; set; }
+        public string ConfirmPassword { get; set; }
+        public string ImageUrl{ get; set; }
+
+    }
+}
